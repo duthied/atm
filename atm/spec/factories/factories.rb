22b4ec72d55d07f7fc1a540d3c16315db2040e12
Devlon Duthie <duthied@gmail.com>
2014-05-03 17:40:04 -0700

    Adds stubs for factorygirl

---
+++ atm/spec/factories/factories.rb
@@ -0,0 +1,15 @@
+# /spec/factories/factories.rb
+FactoryGirl.define do
+
+  factory :user do
+    card_number { Faker::Number.number(9) }
+    pin { Faker::Number.number(4) }
+  end
+
+  # factory :account do
+  #   title { Faker::Lorem.characters(10) }
+  #   balance { Faker::Number.decimal(4,2) }
+  #   user
+  # end
+
+end
---
+++ atm/spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+# /spec/models/user_spec.rb
+require 'spec_helper'
+
+# describe User do
+
+  # it { should respond_to(:card_number) }
+  # it { should respond_to(:pin) }
+
+# end
